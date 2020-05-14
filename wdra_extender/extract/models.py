5ac795d001345e9bd45a1053f2f9ae128db5515c
James Graham <J.Graham@software.ac.uk>
2020-05-14 15:17:13 +0100

    fix: Fix recovery of Tweets from cache

--- wdra_extender/extract/models.py
+++ wdra_extender/extract/models.py
@@ -3,7 +3,6 @@
 import datetime
 import logging
 import json
-from pprint import pprint
 import time
 from uuid import uuid4
 
@@ -54,12 +53,13 @@ class Extract(db.Model):
 
         :param tweet_ids: Tweet IDs to include within this Bundle.
         """
-        logger.info(f'Processing Bundle {self.uuid}')
+        logger.info('Processing Bundle %s', self.uuid)
 
         config = current_app.config
-        r = redis.Redis(host=config['REDIS_HOST'],
-                        port=config['REDIS_PORT'],
-                        db=config['REDIS_DB'])
+        r = redis.Redis(  # pylint: disable=invalid-name
+            host=config['REDIS_HOST'],
+            port=config['REDIS_PORT'],
+            db=config['REDIS_DB'])
 
         uncached_tweet_ids = []
         cached_tweets = []
@@ -73,22 +73,23 @@ class Extract(db.Model):
                 uncached_tweet_ids.append(tweet_id)
 
             else:
-                cached_tweets.append(tweet_string)
+                cached_tweets.append(json.loads(tweet_string))
 
-        logger.info(f'Found {len(cached_tweets)} cached Tweets')
+        logger.info('Found %d cached Tweets', len(cached_tweets))
 
         if uncached_tweet_ids:
-            logger.info(f'Fetching {len(uncached_tweet_ids)} uncached Tweets')
+            logger.info('Fetching %d uncached Tweets', len(uncached_tweet_ids))
             # Twitter API consumer - handles rate limits for us
-            t = Twarc(current_app.config['TWITTER_CONSUMER_KEY'],
-                      current_app.config['TWITTER_CONSUMER_SECRET'])
+            t = Twarc(  # pylint: disable=invalid-name
+                current_app.config['TWITTER_CONSUMER_KEY'],
+                current_app.config['TWITTER_CONSUMER_SECRET'])
 
             time.sleep(10)
 
             # Get data for Tweets not in cache
             # Then put them in the cache for one day
             for tweet in t.hydrate(uncached_tweet_ids):
-                redis_key = f'tweet_hyrated:{tweet["id"]}'
+                redis_key = get_redis_key(tweet['id'])
                 redis_value = json.dumps(tweet)
                 r.setex(redis_key, datetime.timedelta(days=1), redis_value)
 
