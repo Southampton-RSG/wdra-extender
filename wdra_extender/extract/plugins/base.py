3bb8ad99a3c1892e283226d0bf3f75f5e7fed2ea
James Graham <J.Graham@software.ac.uk>
2020-07-29 14:29:24 +0100

    refactor: Refactor logging of plugin subprocesses

--- wdra_extender/extract/plugins/base.py
+++ wdra_extender/extract/plugins/base.py
@@ -1,3 +1,5 @@
+"""Module containing tweet processing plugin loaders and structure."""
+
 import abc
 import importlib
 import logging
@@ -22,21 +24,48 @@ class PluginBase(metaclass=abc.ABCMeta):
         logger.info('Starting plugin: %s', self)
 
     @abc.abstractmethod
-    def run(self, tweets: typing.Iterable):
-        pass
+    def run(self,
+            tweets: typing.Iterable = None,
+            tweets_file: pathlib.Path = None,
+            work_dir: pathlib.Path = None):
+        """Execute this plugin.
+
+        The plugin is expected to save files in the working directory
+        which will be included in the output zip file.
+        """
 
     def __repr__(self) -> str:
         """Representation of plugin for logging purposes."""
         return type(self).__name__
 
 
-def executable_plugin(filepath):
+def log_proc_output(proc: subprocess.CompletedProcess,
+                    level: int = logging.INFO) -> None:
+    """Log stdout and stderr of a subprocess."""
+    logger.log('-- Plugin STDOUT', level=level)
+    for line in proc.stderr.splitlines():
+        logger.log(line, level=level)
+    logger.log('-- End plugin STDOUT', level=level)
+
+    logger.log('-- Plugin STDERR', level=level)
+    for line in proc.stdout.splitlines():
+        logger.log(line, level=level)
+    logger.log('-- End plugin STDERR', level=level)
+
+
+def executable_plugin(filepath) -> typing.Callable:
+    """Factory to construct an Executable Plugin from a filepath.
+
+    An Executable Plugin is any executable program which processes
+    the tweet data provided by it to WDRAX and produces a number
+    of output files in the specified working directory.
+    """
     def run(tweets: typing.Iterable = None,
             tweets_file: pathlib.Path = None,
             work_dir: pathlib.Path = None):
         """Run an executable file as a WDRAX plugin.
 
-        The file is expected to save files in the working directory=
+        The file is expected to save files in the working directory
         which will be included in the output zip file.
         """
         # Add plugin directory to environment so extra files can be used
@@ -44,16 +73,23 @@ def executable_plugin(filepath):
         env['BIN'] = filepath.parent
 
         logger.info('Executing plugin: %s', filepath)
-        proc = subprocess.run([filepath, tweets_file],
-                              cwd=work_dir,
-                              capture_output=True,
-                              env=env,
-                              text=True)
+        try:
+            proc = subprocess.run([filepath, tweets_file],
+                                  cwd=work_dir,
+                                  check=True,
+                                  capture_output=True,
+                                  env=env,
+                                  text=True)
 
-        logger.info('-- Plugin STDERR')
-        for line in proc.stderr.splitlines():
-            logger.info(line)
-        logger.info('-- End plugin STDERR')
+        except subprocess.CalledProcessError:
+            # Process returned non-zero status
+            logger.error('Plugin failed: %s', filepath)
+            log_proc_output(proc, level=logging.ERROR)
+
+            raise
+
+        else:
+            log_proc_output(proc, level=logging.DEBUG)
 
         return proc.stdout
 
@@ -61,6 +97,7 @@ def executable_plugin(filepath):
 
 
 class PluginCollection:
+    """Load plugins from one or multiple directories."""
     def __init__(self, plugin_directories: typing.Iterable[pathlib.Path]):
         self.plugin_directories = [
             d.resolve() for d in plugin_directories if d.is_dir()
@@ -68,6 +105,7 @@ class PluginCollection:
         self.plugins = {}
 
     def load_plugins(self) -> typing.Dict[pathlib.Path, typing.Callable]:
+        """Load plugins from the specified directories."""
         # TODO detect plugins in directory
 
         # Load executable files as plugins
