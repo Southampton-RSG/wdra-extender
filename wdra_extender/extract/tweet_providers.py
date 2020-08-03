abd4b36d3f58f3d21c7f4db99e760cdc4b486974
James Graham <J.Graham@software.ac.uk>
2020-08-03 16:30:10 +0100

    refactor: Clean up Tweet provider code
    Particularly connection to Redis

--- wdra_extender/extract/tweet_providers.py
+++ wdra_extender/extract/tweet_providers.py
@@ -42,15 +42,53 @@ def get_tweets(
     found_tweets = []
 
     for provider in map(import_object, tweet_providers):
-        provider_found_ids, provider_found_tweets = provider(tweet_ids)
+        try:
+            provider_found_ids, provider_found_tweets = provider(tweet_ids)
 
-        found_tweets.extend(provider_found_tweets)
-        tweet_ids -= provider_found_ids
+        except ConnectionError as exc:
+            logger.error('Failed to execute Tweet provider: %s', exc)
+
+        else:
+            found_tweets.extend(provider_found_tweets)
+            tweet_ids -= provider_found_ids
+
         if not tweet_ids:
             logger.info('Found all tweets - skipping remaining providers')
             break
 
+    try:
+        save_to_redis(found_tweets)
+
+    except ConnectionError as exc:
+        logger.error('Failed to cache found Tweets: %s', exc)
+
     return found_tweets
+
+
+def save_to_redis(
+    tweets: typing.List[typing.Mapping],
+    cache_time: datetime.timedelta = datetime.timedelta(days=10)
+) -> None:
+    """Save tweet JSON to Redis cache."""
+    config = current_app.config
+    r = redis.Redis(  # pylint: disable=invalid-name
+        host=config['REDIS_HOST'],
+        port=config['REDIS_PORT'],
+        db=config['REDIS_DB'])
+
+    # Pipeline executes all commands in a single request
+    pipe = r.pipeline()
+    for tweet in tweets:
+        redis_key = f'tweet_hydrated:{tweet["id"]}'
+        redis_value = json.dumps(tweet)
+        # There is no MSETEX command to do this in one request without a pipeline
+        pipe.setex(redis_key, cache_time, redis_value)
+
+    try:
+        pipe.execute()
+
+    except redis.exceptions.ConnectionError as exc:
+        raise ConnectionError from exc
 
 
 def redis_provider(
@@ -67,9 +105,13 @@ def redis_provider(
 
     # Attempt to get all Tweets from cache
     get_redis_key = lambda i: f'tweet_hydrated:{i}'
-    for tweet_id, tweet_string in zip(tweet_ids,
-                                      r.mget(map(get_redis_key, tweet_ids))):
+    try:
+        tweets = r.mget(map(get_redis_key, tweet_ids))
 
+    except redis.exceptions.ConnectionError as exc:
+        raise ConnectionError from exc
+
+    for tweet_id, tweet_string in zip(tweet_ids, tweets):
         if tweet_string is not None:
             found_tweet_ids.add(tweet_id)
 
@@ -84,34 +126,17 @@ def redis_provider(
 def twarc_provider(
     tweet_ids: typing.Iterable[int]
 ) -> typing.Tuple[typing.Set[int], typing.List[typing.Mapping]]:
-    """Get a list of Tweets from their IDs.
+    """Get a list of Tweets from their IDs sourced from the Twitter API.
 
-    First check for Tweets which have been cached, then collect uncached Tweets from the Twitter API.
+    Uses Twarc Twitter API connector - https://github.com/DocNow/twarc.
     """
-    config = current_app.config
-    r = redis.Redis(  # pylint: disable=invalid-name
-        host=config['REDIS_HOST'],
-        port=config['REDIS_PORT'],
-        db=config['REDIS_DB'])
-
     logger.info('Fetching %d uncached Tweets', len(tweet_ids))
     # Twitter API consumer - handles rate limits for us
     t = Twarc(  # pylint: disable=invalid-name
         current_app.config['TWITTER_CONSUMER_KEY'],
         current_app.config['TWITTER_CONSUMER_SECRET'])
 
-    time.sleep(10)
-
-    # Get data for Tweets not in cache
-    # Then put them in the cache
-    found_tweet_ids = set()
-    found_tweets = []
-    for tweet in t.hydrate(tweet_ids):
-        redis_key = f'tweet_hydrated:{tweet["id"]}'
-        redis_value = json.dumps(tweet)
-        r.setex(redis_key, datetime.timedelta(days=10), redis_value)
-
-        found_tweet_ids.add(tweet['id'])
-        found_tweets.append(tweet)
+    found_tweets = list(t.hydrate(tweet_ids))
+    found_tweet_ids = {tweet['id'] for tweet in found_tweets}
 
     return found_tweet_ids, found_tweets
