9c00ba5afdbdc35776be555bec173801d296eea4
Pip Grylls <pipgryllsastro@gmail.com>
2021-02-26 14:11:52 +0000

    debug csv writer

--- wdra_extender/extract/models.py
+++ wdra_extender/extract/models.py
@@ -145,15 +145,6 @@ class Extract(db.Model):
                     csv_writer.writerow(tweet)
                 current_app.logger.info(f'Tweets saved to json')
 
-            with open('names.csv', 'w', newline='') as csvfile:
-                fieldnames = ['first_name', 'last_name']
-                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-                writer.writeheader()
-                writer.writerow({'first_name': 'Baked', 'last_name': 'Beans'})
-                writer.writerow({'first_name': 'Lovely', 'last_name': 'Spam'})
-                writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
-
             """for plugin in get_plugins().values():
                 output = plugin(tweets_file_json, tmp_dir, twitter_key_dict)
                 current_app.logger.info(f'Plugin output: {output}')"""
