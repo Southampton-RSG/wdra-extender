4a0481f0242935a88dfe8999d916be81fbaa0fff
Pip Grylls <pipgryllsastro@gmail.com>
2021-03-09 09:29:47 +0000

    switching to postgres 14

--- wdra_extender/extensions.py
+++ wdra_extender/extensions.py
@@ -20,8 +20,8 @@ login_manager = LoginManager()  # pylint: disable=invalid-name
 def make_celery(_app):
     celery = Celery(
         __name__,
-        backend=f'redis://{_app.config["REDIS HOST"]}:6379/0',
-        broker=f'redis://{_app.config["REDIS HOST"]}:6379/0'
+        backend=f'redis://localhost:6379/0',
+        broker=f'redis://localhost:6379/0'
     )
     celery.conf.update(_app.config)
     TaskBase = celery.Task  # pylint: disable=invalid-name
