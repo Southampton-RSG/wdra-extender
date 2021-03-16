2f8bd78256fbbf044b7545a068eb56a6f09f9ff1
Pip Grylls <pipgryllsastro@gmail.com>
2021-03-16 14:17:31 +0000

    debug user session not persisting 2/n

--- wdra_extender/user/models.py
+++ wdra_extender/user/models.py
@@ -13,8 +13,9 @@ __all__ = [
 
 @login_manager.user_loader
 def load_user(user_id):
+    logger.debug(f"attempting to load user {user_id}")
     # since the user_id is just the primary key of our user table, use it in the query for the user
-    return WdraxUser.query.get(int(user_id))
+    return WdraxUser.query.get(user_id)
 
 
 class WdraxUser(UserMixin, db.Model):
