d9d17cf7e0b3eb1125433c76d31ffe76b20aa829
Pip Grylls <pipgryllsastro@gmail.com>
2021-03-17 16:55:23 +0000

    Getting the neo movie example started

---
+++ wdra_extender/neo/__init__.py
@@ -0,0 +1,9 @@
+"""Package containing functionality for managing the neo4j database views.
+
+Currently used for showing the neo4j example movie database will be used to show and generate twitter
+"""
+from . import neo_view
+
+__all__ = [
+    'neo_view'
+]
